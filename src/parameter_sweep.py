c4785b590f5f855c66ed7e935cefc4c0815f955c
martinjrobins <martinjrobins@gmail.com>
2014-05-19 14:41:34 +0100

    added reduce operation to parameter_sweep.py

--- src/parameter_sweep.py
+++ src/parameter_sweep.py
@@ -5,6 +5,8 @@ import subprocess
 from __builtin__ import range
 import threading
 from math import pi
+import numpy
+import csv
 
 
 k_b = 1.3806488e-23
@@ -15,10 +17,10 @@ D = k_b*T/(3.0*pi*viscosity*diameter);
 
 time = 1e-6
 nout = 500
-#run_dir = "/scratch/robinsonm/git/paper_crowding"
-run_dir = "/home/mrobins/git/paper_crowding"
-#data_dir = "/mi/share/scratch/robinsonm/data/crowding/self_crowding"
-data_dir = "/home/mrobins/tmp"
+run_dir = "/scratch/robinsonm/git/paper_crowding"
+#run_dir = "/home/mrobins/git/paper_crowding"
+data_dir = "/mi/share/scratch/robinsonm/data/crowding/self_crowding"
+#data_dir = "/home/mrobins/tmp"
 
 def run(k_s, sl_div_diam, vol_ratio):
     new_dir = str(k_s)+"_"+str(sl_div_diam)+"_"+str(vol_ratio)
@@ -51,8 +53,43 @@ def run_sweep():
         for thread in threads:
             thread.join()
 
-    
+def reduce(k_s, sl_div_diam, vol_ratio):
+    subdir = str(k_s)+"_"+str(sl_div_diam)+"_"+str(vol_ratio)
+    dir = data_dir + "/" + subdir
+    print "reducing dir ",dir
+    data = numpy.loadtxt(dir+ "/" + 'msd.csv', delimiter=',')
+    valid_range = data[20:,:]
+    t = valid_range[:,1]
+    msd = valid_range[:,2]
+    #flux = valid_range[:,3]
+    A = numpy.array([t,numpy.ones(len(t))])
+    msd_fit = numpy.linalg.lstsq(A.T, msd)[0]
+    #flux_fit = numpy.linalg.lstsq(A.T, flux)[0]
+    return msd_fit[0]/6.0
 
+    
+    
+def reduce_sweep():
+    k_s_sweep = [10**(x-5) for x in range(10)]
+    print "k_s = ",k_s_sweep
+    sl_div_diam_sweep = [5.0*(x+1)/1000 for x in range(20)]
+    print "sl_div_diam = ",sl_div_diam_sweep
+    vol_ratio_sweep = [5.0*(x+1)/100 for x in range(10)]
+    print "vol_ratio = ",vol_ratio_sweep
+
+    params = []
+    for k_s in k_s_sweep:
+        for sl_div_diam in sl_div_diam_sweep:
+            for vol_ratio in vol_ratio_sweep:
+                params.append((k_s,sl_div_diam,vol_ratio))
+    
+    with open(data_dir + "/" + 'D.csv', 'wb') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar='#')
+        for (k_s,sl_div_diam,vol_ratio) in params:
+            Dmsd = reduce(k_s,sl_div_diam,vol_ratio)
+            writer.writerow((k_s,sl_div_diam,vol_ratio,Dmsd))
+    
+        
 
 if __name__ == '__main__':
-    run_sweep()
+    reduce_sweep()
